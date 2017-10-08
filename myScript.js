cee2730573119d81c6b1c3cbb7fdc2ec67e82525
ass <govindaarjoon@gmail.com>
2017-10-08 12:48:17 -0400

    kk

--- myScript.js
+++ myScript.js
@@ -1,10 +1,9 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-var req=httpGet("search.roblox.com/catalog/json")
-print(req)
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       print(xhttp.responseText);
+    }
+};
+xhttp.open("GET", "search.roblox.com/catalog/json", true);
+xhttp.send();
