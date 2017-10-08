5b10772f1d23b5e6a6389fbb507db2d44d50dbda
ass <govindaarjoon@gmail.com>
2017-10-08 12:53:21 -0400

    lol

--- myScript.js
+++ myScript.js
@@ -3,7 +3,8 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
        print(xhttp.responseText);
+       document.getElementsByTagName('P').innerHTML=xhttp.responseText;
     }
 };
-xhttp.open("GET", "search.roblox.com/catalog/json", true);
+xhttp.open("GET", "http://search.roblox.com/catalog/json", true);
 xhttp.send();
